b12efaf99608a48333dcfe20873afea3b1b94f12
AlvinDMartin <AlvinDMartin1999@users.noreply.github.com>
2019-11-22 17:51:22 +0700

    Web bán hàng update 1
    update lan 1

---
+++ WEB_BAN_HANG_MY_PHAM/SourceCode/WebBanHangMyPham/WebBanHangMyPham/Models/DangKy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHangMyPham.Models
+{
+    public class DangKy
+    {
+        [Key]
+        public int MaKhachHang { get; set; }
+        [Required]
+        public string TenDangNhap { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string RetypePassword { get; set; }
+    }
+}
---
+++ WEB_BAN_HANG_MY_PHAM/SourceCode/WebBanHangMyPham/WebBanHangMyPham/Models/DangNhap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHangMyPham.Models
+{
+    public class DangNhap
+    {
+        public int Id { get; set; }
+        [Required]
+        public string TenDangNhap { get; set; }
+        [Required]
+        public string PassWord { get; set; }
+        [Required]
+        public string User { get; set; }
+    }
+}
